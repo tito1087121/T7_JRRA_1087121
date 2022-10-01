7c9cb876eb057a32419476a2848349ba98b9a69b
Titoj <Titoj@TITOSLAPTOP>
2022-10-01 09:14:12 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace T7_JRRA_1087121
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Tarea Laboratorio #7");
+                Console.WriteLine("");
+                Console.WriteLine("José Roberto Rodríguez // 1087121");
+                Console.WriteLine("");
+                Console.WriteLine("Ingrese un número entero mayor a cero");
+                Console.WriteLine("");
+            }
+            catch
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Debe un número entero mayor a cero");
+                Console.WriteLine("");
+            }
+        }
+    }
+}
